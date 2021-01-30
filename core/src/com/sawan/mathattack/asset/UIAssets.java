cfa8e9f5a9f65eb99c33cbac2f78a6b2e543b24e
giorgosk <giorgoskotsikoris@gmail.com>
2021-01-30 14:26:53 +0200

    NinthCommit

--- core/src/com/sawan/mathattack/asset/UIAssets.java
+++ core/src/com/sawan/mathattack/asset/UIAssets.java
@@ -28,6 +28,7 @@ package com.sawan.mathattack.asset;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -45,6 +46,9 @@ public class UIAssets {
 	
 	/** The Constant FILE_UI_SKIN. */
 	private final static String FILE_UI_SKIN = "skin/uiskin.json";
+	
+	/** The Constant CARTWHEEL_FONT_FILE. */
+	private final static String CARTWHEEL_FONT_FILE = "skin/cartwheel.fnt";
 	
 	/** The Constant FILE_IMAGE_BACKGROUND_MAIN. */
 	//private final static String FILE_IMAGE_BACKGROUND_MAIN = "data/ma/game/menu_background.png";
@@ -72,6 +76,20 @@ public class UIAssets {
 	/** The image_main_title. */
 	public static TextureRegion image_main_title;
 	
+	/** The button_level. */
+	public static TextureRegion button_level;
+	
+	/** The image_level_table. */
+	public  static TextureRegion image_level_table;
+	
+	/** The image_level_star. */
+	public static TextureRegion image_level_star;
+	
+	/** The image_level_no_star. */
+	public static TextureRegion image_level_no_star;
+	
+	/** The cartwheel_font. */
+	public static BitmapFont cartwheel_font;
 	
 	/**
 	 * Loads texture file.
@@ -139,20 +157,23 @@ public class UIAssets {
 		image_main_button_settings = getAtlas().findRegion("settings_button");
 		image_main_loader = getAtlas().findRegion("loader");
 		image_main_title = getAtlas().findRegion("title");
+		image_level_table = getAtlas().findRegion("levels_bg");
+		image_level_star = getAtlas().findRegion("star_level");
+		image_level_no_star = getAtlas().findRegion("no_star_level");
 	}
 
 	/**
 	 * Load buttons.
 	 */
 	public static void loadButtons() {
-
+		button_level = getAtlas().findRegion("button_level");
 	}
 
 	/**
 	 * Load fonts.
 	 */
 	public static void loadFonts() {
-
+		cartwheel_font = new BitmapFont(Gdx.files.internal(CARTWHEEL_FONT_FILE));
 	}
 
 	/**
