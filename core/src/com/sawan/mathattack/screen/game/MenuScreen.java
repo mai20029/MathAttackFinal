02375d86c1878f2c9e64d5d34d97187c978e919b
giorgosk <giorgoskotsikoris@gmail.com>
2021-01-30 14:20:27 +0200

    ThirdCommit

--- core/src/com/sawan/mathattack/screen/game/MenuScreen.java
+++ core/src/com/sawan/mathattack/screen/game/MenuScreen.java
@@ -25,78 +25,180 @@
  */
 package com.sawan.mathattack.screen.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.sawan.mathattack.camera.OrthoCamera;
 import com.sawan.mathattack.screen.Screen;
 
 /**
  * The Class MenuScreen.
- *
+ * 
  * @author Sawan J. Kapai Harpalani
  */
 public class MenuScreen extends Screen {
-	
+
 	private OrthoCamera camera;
 
-	/* (non-Javadoc)
+	private final static String BACKGROUND = "menu_back.jpg";
+
+	private Texture background_image;
+
+	private Sprite sprite_background;
+
+	private Stage menu_screen_stage;
+
+	private Skin skin;
+
+	
+	private void createBasicSkin(){
+		  //Create a font
+		  BitmapFont font = new BitmapFont();
+		  skin = new Skin();
+		  skin.add("default", font);
+
+		  //Create a texture
+		  Pixmap pixmap = new Pixmap((int)Gdx.graphics.getWidth()/4,(int)Gdx.graphics.getHeight()/10, Pixmap.Format.RGB888);
+		  pixmap.setColor(Color.WHITE);
+		  pixmap.fill();
+		  skin.add("background",new Texture(pixmap));
+
+		  //Create a button style
+		  TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+		  textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
+		  textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
+		  textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
+		  textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
+		  textButtonStyle.font = skin.getFont("default");
+		  skin.add("default", textButtonStyle);
+
+		}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sawan.mathattack.screen.Screen#create()
 	 */
 	@Override
 	public void create() {
 		camera = new OrthoCamera();
-	}
+		background_image = new Texture(BACKGROUND);
+		sprite_background = new Sprite(background_image);
+		menu_screen_stage = new Stage();
+		
+		createBasicSkin();
+		
+		final TextButton play_button = new TextButton("Play", skin);
+		final TextButton about_button = new TextButton("About", skin);
+		
+		play_button.setWidth(200f);
+		play_button.setHeight(20f);
+		play_button.setPosition(Gdx.graphics.getWidth() / 2 - 100f,
+				Gdx.graphics.getHeight() / 2 - 10f);
+		
+		about_button.setWidth(200f);
+		about_button.setHeight(20f);
+		about_button.setPosition(Gdx.graphics.getWidth() / 2 - 100f,
+				Gdx.graphics.getHeight() / 2 - 50f);
 
-	/* (non-Javadoc)
-	 * @see com.sawan.mathattack.screen.Screen#render(com.badlogic.gdx.graphics.g2d.SpriteBatch)
-	 */
-	@Override
-	public void render(SpriteBatch sprite_batch) {
+		play_button.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				//ScreenManager.setCurrentScreen(new MenuScreen());
+			}
+		});
+
+		menu_screen_stage.addActor(play_button);
+		menu_screen_stage.addActor(about_button);
+		
+		Gdx.input.setInputProcessor(menu_screen_stage);
 		
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.sawan.mathattack.screen.Screen#render(com.badlogic.gdx.graphics.g2d
+	 * .SpriteBatch)
+	 */
+	@Override
+	public void render(SpriteBatch sprite_batch) {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+	    menu_screen_stage.act(Gdx.graphics.getDeltaTime());
+	    
+		sprite_batch.begin();
+		sprite_batch.draw(sprite_background, 0, 0, camera.viewportWidth, camera.viewportHeight);
+		sprite_batch.end();
+		
+		menu_screen_stage.draw();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sawan.mathattack.screen.Screen#resize(int, int)
 	 */
 	@Override
 	public void resize(int width, int height) {
-		camera.resize();
+		//camera.resize();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sawan.mathattack.screen.Screen#dispose()
 	 */
 	@Override
 	public void dispose() {
-		
+		menu_screen_stage.dispose();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sawan.mathattack.screen.Screen#hide()
 	 */
 	@Override
 	public void hide() {
-		
+
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sawan.mathattack.screen.Screen#pause()
 	 */
 	@Override
 	public void pause() {
-		
+
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sawan.mathattack.screen.Screen#resume()
 	 */
 	@Override
 	public void resume() {
-		
+
 	}
 
 	@Override
 	public void update() {
-		camera.update();
+		// camera.update();
 	}
 
 }
