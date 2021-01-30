013db80e703fdc22ebd5a880f8e5272bb638b201
giorgosk <giorgoskotsikoris@gmail.com>
2021-01-30 14:25:47 +0200

    EighthCommit

--- core/src/com/sawan/mathattack/MainStarter.java
+++ core/src/com/sawan/mathattack/MainStarter.java
@@ -25,9 +25,10 @@
  */
 package com.sawan.mathattack;
 
-import com.sawan.mathattack.asset.Assets;
+import com.sawan.mathattack.asset.Backgrounds;
+import com.sawan.mathattack.asset.UIAssets;
 import com.sawan.mathattack.game.AbstractGame;
-import com.sawan.mathattack.game_screens.main.MathAttackMainMenuScreen;
+import com.sawan.mathattack.game_screens.main.MAMainMenuScreen;
 import com.sawan.mathattack.settings.AppSettings;
 
 public class MainStarter extends AbstractGame {
@@ -35,7 +36,7 @@ public class MainStarter extends AbstractGame {
 	@Override
 	public void create() {
         super.create();
-		setScreen(new MathAttackMainMenuScreen(this, "Main Menu"));
+		setScreen(new MAMainMenuScreen(this, "Main Menu"));
 	}
 	
 	@Override
@@ -45,7 +46,8 @@ public class MainStarter extends AbstractGame {
 
 	@Override
 	public void setUpAssets() {
-		Assets.loadAll();
+		UIAssets.loadAll();
+		Backgrounds.loadAll();
 	}
 
 	@Override
